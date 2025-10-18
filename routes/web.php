f3f7bcb17ea5a4ed6224c46e64ce1686aadf9311
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-10-17 21:15:59 -0500

    Revert "Merge branch 'main' of https://github.com/josueCayotopa/facoadiccion"
    This reverts commit 8ecb3269327a6ed618c340254f4f002d21f55e41, reversing
    changes made to 0ab8b1f151054ae6b89c3167b697fbdb98202673.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
+    return view('welcome');
 });
 Route::get('/dashboard', function () {
     return view('dashboard');
